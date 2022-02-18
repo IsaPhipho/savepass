572f002e5e187797fa52883eb28e38b7a8e41624
IsaPhipho <isaphiladelpho@gmail.com>
2022-02-18 00:56:15 +0000

    Challenge finished

--- src/screens/RegisterLoginData/index.tsx
+++ src/screens/RegisterLoginData/index.tsx
@@ -49,7 +49,19 @@ export function RegisterLoginData() {
 
     const dataKey = '@savepass:logins';
 
-    // Save data on AsyncStorage and navigate to 'Home' screen
+    const response = await AsyncStorage.getItem(dataKey);
+    const parsedData = JSON.parse(response) || [];
+    const newLoginListData = [
+      ...parsedData,
+      newLoginData
+    ];
+
+    await AsyncStorage.setItem(
+      dataKey,
+      JSON.stringify(newLoginListData)
+    );
+
+    navigate('Home');
   }
 
   return (
@@ -65,9 +77,8 @@ export function RegisterLoginData() {
             testID="service-name-input"
             title="Nome do serviço"
             name="service_name"
-            error={
-              // Replace here with real content
-              'Has error ? show error message'
+            error={ 
+              errors.service_name && errors.service_name.message 
             }
             control={control}
             autoCapitalize="sentences"
@@ -78,8 +89,7 @@ export function RegisterLoginData() {
             title="E-mail"
             name="email"
             error={
-              // Replace here with real content
-              'Has error ? show error message'
+              errors.email && errors.email.message
             }
             control={control}
             autoCorrect={false}
@@ -91,8 +101,7 @@ export function RegisterLoginData() {
             title="Senha"
             name="password"
             error={
-              // Replace here with real content
-              'Has error ? show error message'
+              errors.password && errors.password.message
             }
             control={control}
             secureTextEntry
